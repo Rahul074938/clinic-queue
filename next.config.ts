fef05fef51c9f68b70c607c1331d7a6644ee0e28
Rahul074938 <rahuldandgey07@gmail.com>
2026-07-11 01:10:43 +0530

    feat: migrate to PostgreSQL to natively resolve vercel read-only database crash

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  outputFileTracingIncludes: {
-    "/:path*": ["./prisma/**/*"],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
